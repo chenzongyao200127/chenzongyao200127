8c65a2c5ef01f72424375fb17ef5e772191635ff
宗璞 <chenzongyao.czy@alibaba-inc.com>
2024-10-30 02:40:10 +0000

    update readme

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 - 🎓 I'm a postgraduate student at the School of Cyber Science and Engineering, SouthEast University (Nanjing).  
 - 💻 My interests lie in system security, Rust, malware detection, program analysis, and operating systems.  
-- 💼 I have interned with the security teams at Tencent and Alibaba Cloud.  
+- 💼 I have interned with the security and OS teams at Tencent and Alibaba Cloud.  
 - 📫 You can reach me at solar1s@163.com.  
 - 😀 Nice to meet you and best wishes for you! :)
 
--- index.js
+++ index.js
@@ -49,9 +49,9 @@ const readme = `\
 
 - 🎓 I'm a postgraduate student at the School of Cyber Science and Engineering, SouthEast University (Nanjing).  
 - 💻 My interests lie in system security, Rust, malware detection, program analysis, and operating systems.  
-- 💼 I have interned with the security teams at Tencent and Alibaba Cloud.  
+- 💼 I have interned with the security & OS teams at Tencent and Alibaba Cloud.  
 - 📫 You can reach me at solar1s@163.com.  
-- 😀 Nice to meet you and best wishes for you! :)
+- 💐 Nice to meet you and best wishes for you! :)
 
 ### GitHub Estatísticas
 
