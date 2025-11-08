95df423ca74935fca13d404c4b45126a575f58e8
Zongyao Chen <ZongYao.Chen@linux.alibaba.com>
2025-11-08 20:10:21 +0800

    update profile
    Signed-off-by: Zongyao Chen <ZongYao.Chen@linux.alibaba.com>

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 // Get the current year
-const thisYear = new Date().getFullYear()
+const thisYear = new Date().getFullYear();
 
 // Calculate the timestamp (milliseconds) at the start of the current year
-const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const startTimeOfThisYear = new Date(
+  `${thisYear}-01-01T00:00:00+00:00`,
+).getTime();
 
 // Calculate the timestamp (milliseconds) at the end of the current year
-const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const endTimeOfThisYear = new Date(
+  `${thisYear}-12-31T23:59:59+00:00`,
+).getTime();
 
 // Calculate the progress ratio from the start of the year to the current moment
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressOfThisYear =
+  (Date.now() - startTimeOfThisYear) /
+  (endTimeOfThisYear - startTimeOfThisYear);
 
 // Call the generateProgressBar function to generate the progress bar for the current year
-const progressBarOfThisYear = generateProgressBar()
+const progressBarOfThisYear = generateProgressBar();
 
 // Define a function to generate the progress bar
 function generateProgressBar() {
   // Set the total length of the progress bar to 30 characters
-  const progressBarCapacity = 30
+  const progressBarCapacity = 30;
 
   // Calculate the length of the progress bar that has passed based on the year's progress
-  const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+  const passedProgressBarIndex = parseInt(
+    progressOfThisYear * progressBarCapacity,
+  );
 
   // Generate the progress bar, with the elapsed time represented by '█' and the remaining time by '▁'
   const progressBar =
-    '█'.repeat(passedProgressBarIndex) +
-    '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
+    "█".repeat(passedProgressBarIndex) +
+    "▁".repeat(progressBarCapacity - passedProgressBarIndex);
 
   // Return the formatted progress bar string
-  return `{ ${progressBar} }`
+  return `{ ${progressBar} }`;
 }
 
 // Create a README string that includes personal information and the year's progress bar
@@ -41,17 +49,17 @@ const readme = `\
 ⏰ Updated on ${new Date().toUTCString()}
 
 ---
-### My GitHub Contributions    
+### My GitHub Contributions
 
-![github-contribution-grid-snake](https://raw.githubusercontent.com/chenzongyao200127/chenzongyao200127/output/github-snake.svg)      
+![github-contribution-grid-snake](https://raw.githubusercontent.com/chenzongyao200127/chenzongyao200127/output/github-snake.svg)
 
-### About me   
+### Welecome to my profile!
 
-- 🎓 I'm a postgraduate student at the School of Cyber Science and Engineering, SouthEast University (Nanjing).  
-- 💻 My interests lie in system security, Rust, malware detection, program analysis, and operating systems.  
-- 💼 I have interned with the security & OS teams at Tencent and Alibaba Cloud.  
-- 📫 You can reach me at solar1s@163.com.  
-- 💐 Nice to meet you and best wishes for you! :)
+- 🎓 Master of Computer Science and Cyber Security.
+- 💻 My interests lie in Operating Systems, System Security and Rust.
+- 💼 Current Working at Alibaba Apsara Lab
+- 📫 You can reach me at solar1s@163.com or chenzongyao200127(wechat)
+- 💐 Glad to connect with you ;)
 
 ### GitHub Estatísticas
 
@@ -59,7 +67,7 @@ const readme = `\
 | ------------- | ------------- |
 
 \
-`
+`;
 
 // Print the generated README content to the console
-console.log(readme)
+console.log(readme);
